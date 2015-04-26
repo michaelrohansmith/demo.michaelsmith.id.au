adf1c70be124f692f975958eb0366cb974c7bdc4
Michael Smith <michael.smith@coinsetter.com>
2015-04-26 17:35:18 +1000

    Should be possible to modify a task and leave next task blank

--- demo/demo/models.py
+++ demo/demo/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Task(models.Model):
 
     name = models.CharField(max_length=20)
-    next = models.ForeignKey('self', null=True)
+    next = models.ForeignKey('self', null=True, default=None, blank=True)
     is_active = models.BooleanField(default=False)
     start_here = models.BooleanField(default=False)
     
